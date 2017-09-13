2f2447d540cee5897bce2f11f1093e0b333d2da3
Hung Huynh <hung.huynh@payfactors.com>
2017-09-12 23:36:24 -0400

    Timer Clock

---
+++ DigitalClock/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        Timer t = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Timer interval
+            t.Interval = 1000; //in milliseconds
+
+            t.Tick += new EventHandler(this.t_Tick);
+
+            // Start timer when form loads
+            t.Start(); // This will use t_Tick() method
+        }
+        // Timer eventhandler
+        private void t_Tick(object sender, EventArgs e)
+        {
+            string clock = DateTime.Now.ToString("HH:mm:ss");
+            // Update label
+            label1.Text = clock;
+        }
+    }
+}
